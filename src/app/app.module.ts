ef42780582d6dc89ad326cfbba15743a8d22c2b5
Amandeep Singh <Amandeep.Singh4@transport.nsw.gov.au>
2021-07-18 16:41:04 +1000

    Adding Promise component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './includes/header/header.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { PromiseComponent } from './promise/promise.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    PromiseComponent
   ],
   imports: [
     BrowserModule,
