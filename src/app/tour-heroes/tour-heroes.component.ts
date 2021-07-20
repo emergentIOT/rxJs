cc26de9ffc3ae8aa0bcf173ea69c7e7b9dd09722
Amandeep Singh <Amandeep.Singh4@transport.nsw.gov.au>
2021-07-21 09:59:47 +1000

    Adding ngFor

--- src/app/tour-heroes/tour-heroes.component.ts
+++ src/app/tour-heroes/tour-heroes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../interface/Hero'
+import { HEROES } from '../mock-heroes';
+
 //This specify angular meta data.
 @Component({
   selector: 'app-tour-heroes',
@@ -10,17 +12,19 @@ export class TourHeroesComponent implements OnInit {
 
   birthday = new Date(1988, 3, 15);
   title = 'Tour of heroes';
-  
+
   hero: Hero = {
     id: 1,
     name: 'wisdom',
-   
   }
+
+  //export the dummy data.
+  heroes = HEROES;
  
   constructor() { }
 
   //This is called shortly after creating the component
   ngOnInit(): void {
   }
-
+ 
 }
